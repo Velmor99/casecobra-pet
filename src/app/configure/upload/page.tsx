79c25d9ae7f0fa1e3fef686f2ac81883bbdb3d46
Velmor99 <wincentwangog@gmail.com>
2024-08-08 23:51:53 +0300

    done ui part of upload image page

--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Progress } from "@/components/ui/progress";
 import { cn } from "@/lib/utils";
 import { Image, Loader2, MousePointerSquareDashed } from "lucide-react";
 import { useState, useTransition } from "react";
@@ -7,6 +8,7 @@ import Dropzone, { FileRejection } from "react-dropzone";
 
 const Page = () => {
   const [isDragOver, setIsDragOver] = useState<boolean>(false);
+  const [uploadProgress, setUploadProgress] = useState(0);
 
   const onDropRejected = () => {};
   const onDropAccepted = () => {
@@ -50,6 +52,35 @@ const Page = () => {
               ) : (
                 // eslint-disable-next-line jsx-a11y/alt-text
                 <Image className="h-6 w-6 text-zinc-500 mb-2" />
+              )}
+              <div className="flex flex-col justify-center mb-2 text-sm text-zinc-700">
+                {isUploading ? (
+                  <div className="flex flex-col items-center">
+                    <p>Uploading...</p>
+                    <Progress
+                      value={uploadProgress}
+                      className="mt-2 w-40 bg-gray-300"
+                    />
+                  </div>
+                ) : isPending ? (
+                  <div className="flex flex-col items-center">
+                    <p>Redirecting, please wait...</p>
+                  </div>
+                ) : isDragOver ? (
+                  <p>
+                    <span className="font-semibold">Drop file</span>
+                    to upload
+                  </p>
+                ) : (
+                  <p>
+                    <span className="font-semibold">Click to upload</span> or
+                    drag and drop
+                  </p>
+                )}
+              </div>
+
+              {isPending ? null : (
+                <p className="text-xs text-zinc-500">.PNG, .JPG, .JPEG</p>
               )}
             </div>
           )}
