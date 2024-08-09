095ca063aeb1443597c1fac17b8997d5aae9a834
Velmor99 <wincentwangog@gmail.com>
2024-08-09 04:50:08 +0300

    done steps component

--- src/app/configure/layout.tsx
+++ src/app/configure/layout.tsx
@@ -1,9 +1,13 @@
 import MaxWidhWrapper from "@/components/MaxWithWrapper";
+import Steps from "@/components/Steps";
 import { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <MaxWidhWrapper className="flex-1 flex flex-col">{children}</MaxWidhWrapper>
+    <MaxWidhWrapper className="flex-1 flex flex-col">
+      <Steps />
+      {children}
+    </MaxWidhWrapper>
   );
 };
 
