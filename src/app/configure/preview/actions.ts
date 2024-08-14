be2ce48bc3e70e8c29667745d86c6873bc777b07
Velmor99 <wincentwangog@gmail.com>
2024-08-14 06:12:43 +0300

    done configuration-summary-page

---
+++ src/app/configure/preview/actions.ts
@@ -0,0 +1,13 @@
+"use server";
+
+import { db } from "@/db";
+
+export const createCheckoutSession = async ({
+  configId,
+}: {
+  configId: string;
+}) => {
+  const configuration = await db.configuration.findUnique({
+    where: { id: configId },
+  });
+};
